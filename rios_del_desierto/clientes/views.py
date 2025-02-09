2959c533de1d1b8a2e1c7e00d426e75c72060efb
Alejandro Amaya <tresmaster@protonmail.ch>
2025-02-09 01:25:14 -0500

    upd: vistas

--- rios_del_desierto/clientes/views.py
+++ rios_del_desierto/clientes/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+# clientes/views.py
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from .models import Cliente
+from .serializers import ClienteSerializer
 
-# Create your views here.
+@api_view(['GET'])
+def buscar_cliente_por_documento(request, numero_documento):
+    try:
+        cliente = Cliente.objects.get(numero_documento=numero_documento)
+        serializer = ClienteSerializer(cliente)
+        return Response(serializer.data, status=200)
+    except Cliente.DoesNotExist:
+        return Response({"error": "Cliente no encontrado."}, status=404)
