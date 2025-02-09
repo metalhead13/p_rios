319da191f6222a4f1f719437d0ecfa6348632c6c
Alejandro Amaya <tresmaster@protonmail.ch>
2025-02-09 00:13:42 -0500

    Hello friend!

---
+++ rios_del_desierto/rios_del_desierto/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for rios_del_desierto project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/5.1/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'rios_del_desierto.settings')
+
+application = get_asgi_application()
